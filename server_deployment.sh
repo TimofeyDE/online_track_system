489bc8deb5ca858fb0824f46309ad7dbe02ff536
TimofeyDE <timofey.dankevich@gmail.com>
2024-02-10 19:45:47 +0200

    Added an opportunity to run either in background or foreground the docker containers

--- server_deployment.sh
+++ server_deployment.sh
@@ -1,10 +1,33 @@
 #!/bin/bash
 
-sudo docker-compose pull
+# Check if at least one argument is provided
+if [ $# -eq 0 ]; then
+    echo "Usage: $0 -f[foreground] or -b[background]"
+    exit 1
+fi
 
-# Build a Docker
-sudo docker-compose build
+while getopts "fb" OPT;
+do
+    case "${OPT}" in
+        f)
+            sudo docker-compose pull
+            sudo docker-compose build
+            sudo docker-compose up
+            exit 0
+            ;;
+        b)
+            sudo docker-compose pull -q
 
-# Run a Docker
-sudo docker-compose up
+            # Build a Docker in background
+            sudo docker-compose build -q
+
+            # Run a Docker in background
+            sudo docker-compose up -d
+            exit 0
+            ;;
+        *)
+            exit 1
+            ;;
+    esac
+done
 
